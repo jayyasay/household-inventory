49a247017be241522e68312ad836b6600ff722aa
unknown <paolo_yasay@yahoo.com>
2021-12-12 20:08:42 +0400

    Setup household on personal PC

--- src/index.js
+++ src/index.js
@@ -1,88 +1,10 @@
-import React, {useState} from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
-import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
-import PreviewIcon from '@mui/icons-material/Preview';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
-const drawerWidth = 200;
-
-export default function ClippedDrawer() {
-  const paraOne = "The quick brown fox jumps over the lazy dog!"
-  const paraTwo = "Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Or sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit"
-  const [para, setPara] = useState(paraOne);
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-        <AssignmentTurnedInOutlinedIcon fontSize="large" />
-          <Typography variant="h6" noWrap component="div">
-            Web Household Inventory
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            <ListItem button onClick={() => setPara(paraOne)} sx={{display: 'flex', flexDirection: 'row', columnGap: 2, paddingLeft: 1}}>
-              <AddToPhotosIcon />
-              <ListItemText primary="Photos"/>
-            </ListItem>
-            <ListItem button onClick={() => setPara(paraTwo)} sx={{display: 'flex', flexDirection: 'row', columnGap: 2, paddingLeft: 1}}>
-                <PreviewIcon />
-                <ListItemText primary="Work" />
-            </ListItem>
-      <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-          <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" />
-      </ListItem>
-          </List>
-          <Divider />
-        </Box>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        <Typography paragraph>
-          {para}
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
-    </Box>
-  );
-}
+ReactDOM.render(
+	<React.StrictMode >
+	<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
